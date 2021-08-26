4b5d881244bfa39b5fbfecf0e0f03d0b150300f4
TheHebi <nturcotte8@live.com>
2021-08-25 17:20:49 -0700

    user routes

--- models/index.js
+++ models/index.js
@@ -4,12 +4,42 @@ const Comment = require("./Comment");
 const Budget = require("./Budget");
 const Plan = require("./Plan");
 
+Comment.belongsTo(Trip);
+Trip.hasMany(Comment);
 
+Comment.belongsTo(Plan);
+Plan.hasMany(Comment);
+
+Trip.hasMany(Plan);
+Plan.belongsTo(Trip);
+
+Trip.belongsToMany(User, {
+  through: `UserTrip`,
+});
+User.belongsToMany(Trip, {
+  through: `UserTrip`,
+});
+
+Plan.belongsToMany(User, {
+  through: `UserPlan`,
+});
+User.belongsToMany(Plan, {
+  through: `UserPlan`,
+});
+
+Budget.belongsTo(Trip);
+Trip.hasOne(Budget);
+
+Budget.belongsTo(Plan);
+Plan.hasOne(Budget);
+
+Budget.belongsTo(User);
+User.hasMany(Budget);
 
 module.exports = {
-    User,
-    Trip,
-    Comment,
-    Budget,
-    Plan,
-}
+  User,
+  Trip,
+  Comment,
+  Budget,
+  Plan,
+};
