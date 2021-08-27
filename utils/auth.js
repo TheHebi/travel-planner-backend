baf3ad0bcc5254e91921374c111c7f092dfa8213
TheHebi <nturcotte8@live.com>
2021-08-27 13:30:44 -0700

    auth fix

--- utils/auth.js
+++ utils/auth.js
@@ -1,33 +1,20 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
-const secret = process.env.JWT_SECRET;
-const expiration = "2h";
-
-module.exports = {
-  authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
-
-    if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
-    }
-
-    if (!token) {
-      return req;
-    }
-
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log("Invalid token");
-    }
-
-    return req;
-  },
-
-  signToken: function ({ email, username, _id }) {
-    const payload = { email, username, _id };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+const tokenAuth = (req, res, next) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.status(403).json({ message: `Auth Failed!` });
+      } else {
+        req.user = data;
+        return next();
+      }
+    });
+  } else {
+    return res.status(403).json({ message: `Auth Failed!` });
+  }
 };
+
+module.exports = tokenAuth;
